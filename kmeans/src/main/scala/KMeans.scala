372e4d611110dc7234ed2a868eb271b44595fe09
vandysnape <venkatesh.vas@husky.neu.edu>
2019-12-06 12:32:18 -0800

    10iterationschanges

--- kmeans/src/main/scala/KMeans.scala
+++ kmeans/src/main/scala/KMeans.scala
@@ -56,55 +56,61 @@ object KMeans {
     val data = lines.map(parseVector _).cache()
 
 
-        val K = args(1).toInt
+
+
+        var K = 1
+
         val convergeDist = args(2).toDouble
 
-        val kPoints = data.takeSample(withReplacement = false, K, 42)
-        var tempDist = 1.0
-        var closest = data.map(p => (closestPoint(p, kPoints), (p, 1)))
 
-        while (tempDist > convergeDist) {
-          closest = data.map(p => (closestPoint(p, kPoints), (p, 1)))
-          println("Closest points")
+        while(K<=10) {
+          val kPoints = data.takeSample(withReplacement = false, K, 42)
+          var tempDist = 1.0
+          var closest = data.map(p => (closestPoint(p, kPoints), (p, 1)))
+
+          while (tempDist > convergeDist) {
+            closest = data.map(p => (closestPoint(p, kPoints), (p, 1)))
+            println("Closest points")
+            closest.foreach(println)
+            //closest.saveAsTextFile("Closest")
+
+            val pointStats = closest.reduceByKey { case ((p1, c1), (p2, c2)) => (p1 + p2, c1 + c2) }
+            pointStats.foreach(println)
+            //pointStats.saveAsTextFile("CenterAndSum")
+            val newP = pointStats.map { pair =>
+              (pair._1, pair._2._1 * (1.0 / pair._2._2))
+            }
+            //  newP.saveAsTextFile("NewPoints")
+
+            val newPoints = pointStats.map { pair =>
+              (pair._1, pair._2._1 * (1.0 / pair._2._2))
+            }.collectAsMap()
+
+
+            tempDist = 0.0
+            for (i <- 0 until K) {
+              tempDist += squaredDistance(kPoints(i), newPoints(i))
+            }
+
+            for (newP <- newPoints) {
+              kPoints(newP._1) = newP._2
+            }
+            if (tempDist > convergeDist) {
+
+            }
+
+
+            println(s"Finished iteration "+K+" (delta = $tempDist)")
+          }
+
+          println("**************************Iteration "+K+" **********************")
+          println("Final centers:")
+          kPoints.foreach(println)
+          println("Points and centers")
           closest.foreach(println)
-          //closest.saveAsTextFile("Closest")
-
-          val pointStats = closest.reduceByKey { case ((p1, c1), (p2, c2)) => (p1 + p2, c1 + c2) }
-          pointStats.foreach(println)
-          //pointStats.saveAsTextFile("CenterAndSum")
-          val newP = pointStats.map { pair =>
-            (pair._1, pair._2._1 * (1.0 / pair._2._2))
-          }
-        //  newP.saveAsTextFile("NewPoints")
-
-          val newPoints = pointStats.map { pair =>
-            (pair._1, pair._2._1 * (1.0 / pair._2._2))
-          }.collectAsMap()
-
-
-          tempDist = 0.0
-          for (i <- 0 until K) {
-            tempDist += squaredDistance(kPoints(i), newPoints(i))
-          }
-
-          for (newP <- newPoints) {
-            kPoints(newP._1) = newP._2
-          }
-          if(tempDist > convergeDist){
-
-          }
-
-
-          println(s"Finished iteration (delta = $tempDist)")
+          K = K+1
         }
 
-        println("Final centers:")
-        kPoints.foreach(println)
-        println("Points and centers")
-        closest.foreach(println)
-       // data.saveAsTextFile("output")
-   // val size = data.map(x => x.length)
-    //size.saveAsTextFile("output")
     spark.stop()
   }
 }
